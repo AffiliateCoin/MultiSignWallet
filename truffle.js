1345b689a75ab8aafe39a3bcb2be626db4e27c30
riccardo <ravaro.riccardo@gmail.com>
2018-02-07 14:09:14 +0700

    live version

--- truffle.js
+++ truffle.js
@@ -14,6 +14,7 @@ module.exports = {
     "dev": {
       host: '127.0.0.1',
       port: 7545,
+      gas: 4712388,
       network_id: 5777, // Match any network id
       from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57"
     },
@@ -28,15 +29,16 @@ module.exports = {
       network_id: 3,
       host: "127.0.0.1",
       port: 8545,
-      gas: 4712388,
-      gasPrice: 25000000000,
+      gas: 4700036,
+      gasPrice:  20000000000,
       from: "0x749d3dcefe46d71a4eb004774efd5dc2bd468d8b"
     },
     "mainnetparity": {
       network_id: 1,
       host: "127.0.0.1",
       port: 8545,
-      gas: 6712390,
+      gas: 1678298,
+      gasPrice: 25000000000,
       from: "0x7db3cecaa8abb4a4dfac8c5d85c6138799978d99"
     }
   },
